8180de453d8c04b01e0a586f1f9479e8e9f7c337
xiez <xiezhen@qdedu.cn>
2020-06-28 18:04:42 +0800

    保存

--- spider/spiders/weather.py
+++ spider/spiders/weather.py
@@ -1,7 +1,18 @@
 import scrapy
 
 class WeatherSpider(scrapy.Spider):
-    name = 'weather'
+    name = 'price'
     start_urls = [
-        '',
+        'http://www.beijingprice.cn/jfcx/sp/index.shtml?ItemCode=E000002',
     ]
+
+    def parse(self, response):
+        # blue = response.css('div.blue-text')
+        # green = response.css('div.green-text')
+        # red = response.css('div.red-text')
+        yield {
+            'blueRice':response.css('#pricePF::text').get(),
+            'greenPrice':response.css('#priceLS::text').get(),
+            'redPrice':response.css('#priceCS::text').get(),
+        }
+
